241e1e574a7e54c43659b9e8b990ee77549cb290
JamesSweetland <james.sweetland@live.co.uk>
2022-02-28 19:27:42 +0000

    Added accept and reject buttons

--- components/AppScreens/RequestsScreen.js
+++ components/AppScreens/RequestsScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, FlatList} from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class RequestsScreen extends Component{
@@ -55,9 +55,35 @@ class RequestsScreen extends Component{
           throw 'Something went wrong';
         }
       })
-      .then((responseJson) => {
-        if(responseJson.length != 0){            
-          this.setState({ requests: responseJson });
+      .then((responseJson) => {          
+        this.setState({ requests: responseJson });
+      })
+    }
+    catch(error){
+      console.error(error);
+    }
+  }
+
+  handleRequest = (methodType, userID) => {
+    try{
+      let sessionToken = this.state.token;
+
+      return fetch("http://localhost:3333/api/1.0.0/friendrequests/" + userID, {
+        method: methodType,
+        headers: {
+          'X-Authorization': sessionToken
+        }
+      })
+      .then((response) => {
+        //checks the response code before returning the json
+        if(response.status === 200){
+          console.log(methodType + 'ED request');
+        }else if(response.status === 401){//if not authorised then redirect to login
+          this.props.navigation.navigate("Login");
+        }else if(response.status === 401){
+          throw 'User Not Found';
+        }else{
+          throw 'Something went wrong';
         }
       })
     }
@@ -66,37 +92,59 @@ class RequestsScreen extends Component{
     }
   }
 
-  handleRequest = (method) => {
-    try{
-      return fetch
-    }
-    catch(error){
-      console.error(error);
-    }
-  }
-
   render(){
-    return(
-      <View style={styles.container}>
-        
-        <View style={{ alignItems: 'center' }}>
-          <Text style={styles.title}>SpaceBook</Text>
-          <Text style={styles.text}>Requests Screen</Text>
-        </View>        
+    if(this.state.requests.length == 0){
+      return(
+        <View style={styles.container}>
+          
+          <View style={{ alignItems: 'center' }}>
+            <Text style={styles.title}>SpaceBook</Text>
+            <Text style={styles.text}>Requests Screen</Text>
+          </View>        
 
-        <FlatList
-          data={this.state.requests}
-          renderItem={({item}) => (
-            <View style={ styles.requestsContainer }>          
-              <Text>{item.first_name} {item.last_name}</Text>
-            </View>
-          )}
-          keyExtractor={(item,index) => item.user_id.toString()}
-          style={{ padding: 5 }}
-        />  
-    
-      </View>
-    );
+          <View style={{ alignItems: 'center', marginTop: '30vh' }}>
+            <Text style={styles.text}>No Requests</Text>  
+          </View>          
+      
+        </View>
+      );
+    }
+    else{
+      return(
+        <View style={styles.container}>
+          
+          <View style={{ alignItems: 'center' }}>
+            <Text style={styles.title}>SpaceBook</Text>
+            <Text style={styles.text}>Requests Screen</Text>
+          </View>        
+  
+          <FlatList
+            data={this.state.requests}
+            renderItem={({item}) => (
+              <View style={ styles.requestsContainer }>          
+                <Text>{item.first_name} {item.last_name}</Text>
+  
+                <View style={{ flexDirection: 'row' }}>
+                  <Button
+                    title='Accept'
+                    onPress={() =>this.handleRequest('POST', item.user_id)}
+                    color="#19a9f7"
+                  />
+                  <Button
+                    title='Reject'
+                    onPress={() =>this.handleRequest('DELETE', item.user_id)}
+                    color="#19a9f7"
+                  />
+                </View>              
+              </View>
+            )}
+            keyExtractor={(item,index) => item.user_id.toString()}
+            style={{ padding: 5 }}
+          />  
+      
+        </View>
+      );
+    }    
   }
 }
 
@@ -108,7 +156,7 @@ const styles = StyleSheet.create({
   title: {
     color: '#19a9f7',
     fontWeight: 'bold',
-    fontSize: '400%',
+    fontSize: 'min(16vw, 500%)'//css sets title to 16% of the viewpoint width but never more than the font size 500%
   },
   text: {
     padding: 5,
